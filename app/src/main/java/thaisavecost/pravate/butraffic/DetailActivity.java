4597644424e1b9b59bb2ca8fe034ccd9da93f2e6
pravateb <pravateb.gmail.com>
2015-11-13 14:16:20 +0700

    Exersice Success

--- app/src/main/java/thaisavecost/pravate/butraffic/DetailActivity.java
+++ app/src/main/java/thaisavecost/pravate/butraffic/DetailActivity.java
@@ -1,13 +1,51 @@
 package thaisavecost.pravate.butraffic;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static thaisavecost.pravate.butraffic.R.id.imageView2;
 
 public class DetailActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView titleTextView, detailTextView;
+    private ImageView trafficImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        bindWidget();
+
+        // Receive Value from Intent
+        receiveFromIntent();
+
+    }  // Main Method
+
+    private void receiveFromIntent() {
+
+        String strTitle = getIntent().getStringExtra("Title");
+        titleTextView.setText(strTitle);
+
+        int intImage = getIntent().getIntExtra("Image", R.drawable.traffic_01); // 01 เป็นค่า Default กรณีรับค่าไม่ได้
+        trafficImageView.setImageResource(intImage);
+
+        String[] strDetail = getResources().getStringArray(R.array.detail);
+        int intIndex = getIntent().getIntExtra("Index", 0);
+        detailTextView.setText(strDetail[intIndex]);
+
     }
-}
+
+    private void bindWidget() {
+
+        titleTextView = (TextView) findViewById(R.id.textView3);
+        detailTextView = (TextView) findViewById(R.id.textView4);
+        trafficImageView = (ImageView) findViewById(R.id.imageView2);
+
+    }
+
+}  // Main Class
--- app/src/main/java/thaisavecost/pravate/butraffic/MainActivity.java
+++ app/src/main/java/thaisavecost/pravate/butraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -39,10 +40,14 @@ public class MainActivity extends AppCompatActivity {
 
     }   // Main Method จบด้วย ; เสมอ และ ต้องมี onCreate เสมอ
 
+    public void clickExercise(View view) {
+        startActivity(new Intent(MainActivity.this, ExerciseActivity.class));
+    }
+
     private void createListview() {
 
         //Setup Array
-        String[] strTitle = new String[20];
+        final String[] strTitle = new String[20];
         strTitle[0] = "ห้ามเลี้ยวซ้าย";
         strTitle[1] = "ห้ามเลี้ยวขวา";
         strTitle[2] = "ตรงไป";
@@ -64,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         strTitle[18] = "จำกัดความกว้าง";
         strTitle[19] = "จำกัดความสูง";
 
-        int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03,
+        final int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03,
                 R.drawable.traffic_04, R.drawable.traffic_05, R.drawable.traffic_06,
                 R.drawable.traffic_07, R.drawable.traffic_08, R.drawable.traffic_09,
                 R.drawable.traffic_10, R.drawable.traffic_11, R.drawable.traffic_12,
@@ -74,6 +79,19 @@ public class MainActivity extends AppCompatActivity {
 
         MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, strTitle, intIcon);
         trafficListView.setAdapter(objMyAdapter);
+
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent objIntent = new Intent(MainActivity.this, DetailActivity.class);
+                objIntent.putExtra("Title", strTitle[position]);
+                objIntent.putExtra("Image", intIcon[position]);
+                objIntent.putExtra("Index", position);
+                startActivity(objIntent);
+
+            }  // event
+        });
 
     } // Create ListView
 
