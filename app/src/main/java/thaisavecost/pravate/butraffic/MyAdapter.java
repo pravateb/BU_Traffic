2e03d182fd47804b85b2522d796c02fd86fadc29
pravateb <pravateb.gmail.com>
2015-11-13 11:12:47 +0700

    ListView Success 2

--- app/src/main/java/thaisavecost/pravate/butraffic/MyAdapter.java
+++ app/src/main/java/thaisavecost/pravate/butraffic/MyAdapter.java
@@ -1,9 +1,15 @@
 package thaisavecost.pravate.butraffic;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+import static thaisavecost.pravate.butraffic.R.id.textView2;
 
 /**
  * Created by student on 11/12/2015.
@@ -23,7 +29,7 @@ public class MyAdapter extends BaseAdapter{
 
     @Override
     public int getCount() {
-        return 0;
+        return titleStrings.length;
     }
 
     @Override
@@ -38,6 +44,18 @@ public class MyAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView1 = objLayoutInflater.inflate(R.layout.my_listview, parent, false);
+
+        //Setup Title
+        TextView titleTextView = (TextView) objView1.findViewById(R.id.textView2);
+        titleTextView.setText(titleStrings[position]);
+
+        //Setup Icon
+        ImageView iconImageView = (ImageView) objView1.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[position]);
+
+        return objView1;
     }
 }   // Main Class
